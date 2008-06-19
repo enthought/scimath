26925d29726b81a3bf7c72dc170171f87a9c03b6
oliphant <oliphant@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-06-19 20:04:38 +0000

    Add more angle units.

--- enthought/units/angle.py
+++ enthought/units/angle.py
@@ -33,5 +33,41 @@ degree.label = 'deg'
 degrees = degree
 deg = degree
 
-radian.label = 'radians'
+radian.label = 'rad'
 radians = radian
+
+grad = degree * 0.9
+grad.label = '^g'
+grads = grad
+gon = grad
+gons = grad
+
+minute = degree / 60.0
+minutes = minute
+minute.label = "'"
+second = degree / 3600.0
+second.label = '"'
+seconds = second
+
+sign = degree * 30
+signs = sign
+
+revolution = 360 * degree
+revolution.label = 'r'
+revolutions = revolution
+circle = revolution
+circles = circle
+turn = circle
+turns = circles
+
+quadrant = 90 * degree
+quadrants = quadrant
+right_angle = quadrant
+right_angles = quadrant
+
+sextant = 60*degree
+sextants = sextant
+
+mil = 90 / 1600.0 * degree
+mil.label = 'mil'
+mils = mil
