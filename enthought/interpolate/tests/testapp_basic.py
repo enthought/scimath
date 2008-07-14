ef5ea7ed9ecda063c6d7364ef20194ea00b98de5
ischnell <ischnell@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-14 17:22:55 +0000

    Made this test a proper unittest

--- enthought/interpolate/tests/testapp_basic.py
+++ enthought/interpolate/tests/testapp_basic.py
@@ -1,51 +1,73 @@
+import unittest
+import time
 
-from enthought.interpolate.api import *
+from numpy import arange, ones, allclose
+import scipy
 
-def main():
-    from numpy import arange, ones
-    import time
-    N = 3000.
-    x = arange(N)
-    y = arange(N)
-    new_x = arange(N)+0.5
-    t1 = time.clock()
-    new_y = linear(x, y, new_x)
-    t2 = time.clock()
-    print '1d interp (sec):', t2 - t1
-    print new_y[:5]
+from enthought.interpolate.api import linear, block_average_above
 
-    N = 3000.
-    x = arange(N)
-    y = arange(N)
 
-    new_x = arange(N/2)*2
-    t1 = time.clock()
-    new_y = block_average_above(x, y, new_x)
-    t2 = time.clock()
-    print '1d block_average_above (sec):', t2 - t1
-    print new_y[:5]
+class Test(unittest.TestCase):
     
-    N = 3000.
-    x = arange(N)
-    y = ones((100,N)) * arange(N)
-    new_x = arange(N)+0.5
-    t1 = time.clock()
-    new_y = linear(x, y, new_x)
-    t2 = time.clock()
-    print 'fast interpolate (sec):', t2 - t1
-    print new_y[:5,:5]
+    def assertAllclose(self, x, y):
+        self.assert_(allclose(x, y))
+        
+    def test_linear(self):
+        N = 3000.
+        x = arange(N)
+        y = arange(N)
+        new_x = arange(N)+0.5
+        t1 = time.clock()
+        new_y = linear(x, y, new_x)
+        t2 = time.clock()
+        print '1d interp (sec):', t2 - t1
+        self.assertAllclose(new_y[:5], [0.5, 1.5, 2.5, 3.5, 4.5])
 
-    import scipy
-    N = 3000.
-    x = arange(N)
-    y = ones((100, N)) * arange(N)
-    new_x = arange(N)
-    t1 = time.clock()
-    interp = scipy.interpolate.interp1d(x, y)
-    new_y = interp(new_x)
-    t2 = time.clock()
-    print 'scipy interp1d (sec):', t2 - t1
-    print new_y[:5,:5]
+    def test_block_average_above(self):
+        N = 3000.
+        x = arange(N)
+        y = arange(N)
+        
+        new_x = arange(N/2)*2
+        t1 = time.clock()
+        new_y = block_average_above(x, y, new_x)
+        t2 = time.clock()
+        print '1d block_average_above (sec):', t2 - t1
+        self.assertAllclose(new_y[:5], [0.0, 0.5, 2.5, 4.5, 6.5])
+
+    def test_linear2(self):
+        N = 3000.
+        x = arange(N)
+        y = ones((100,N)) * arange(N)
+        new_x = arange(N)+0.5
+        t1 = time.clock()
+        new_y = linear(x, y, new_x)
+        t2 = time.clock()
+        print 'fast interpolate (sec):', t2 - t1
+        self.assertAllclose(new_y[:5,:5],
+                            [[ 0.5, 1.5, 2.5, 3.5, 4.5],
+                             [ 0.5, 1.5, 2.5, 3.5, 4.5],
+                             [ 0.5, 1.5, 2.5, 3.5, 4.5],
+                             [ 0.5, 1.5, 2.5, 3.5, 4.5],
+                             [ 0.5, 1.5, 2.5, 3.5, 4.5]])
+
+    def test_interp1d(self):
+        N = 3000.
+        x = arange(N)
+        y = ones((100, N)) * arange(N)
+        new_x = arange(N)
+        t1 = time.clock()
+        interp = scipy.interpolate.interp1d(x, y)
+        new_y = interp(new_x)
+        t2 = time.clock()
+        print 'scipy interp1d (sec):', t2 - t1
+        self.assertAllclose(new_y[:5,:5],
+                            [[ 0,  1,  2,  3,  4],
+                             [ 0,  1,  2,  3,  4],
+                             [ 0,  1,  2,  3,  4],
+                             [ 0,  1,  2,  3,  4],
+                             [ 0,  1,  2,  3,  4]])
+
 
 if __name__ == '__main__':
-    main()
+    unittest.main()
