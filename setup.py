7e88e96fb4036e7c30980ed47027bfd819251221
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-30 17:47:30 +0000

    * Added / edited project meta-data in preparation for pushing into PyPi.

--- setup.py
+++ setup.py
@@ -15,16 +15,30 @@ calculations, beyond the capabilities offered by SciPy.
 - enthought.units
 
 """
-import os,setuptools, zipfile
-from numpy.distutils.core import setup
-from setuptools.command.develop import develop
-from distutils.command.build import build as distbuild
+
+
+# NOTE: Setuptools must be imported BEFORE numpy.distutils or else
+# numpy.distutils won't do the correct thing.
+import setuptools
+
+
 from distutils import log
-from pkg_resources import DistributionNotFound, parse_version, require, VersionConflict 
-
+from distutils.command.build import build as distbuild
+from make_docs import HtmlBuild
+from numpy.distutils.core import setup
+from pkg_resources import DistributionNotFound, parse_version, require, \
+    VersionConflict
 from setup_data import INFO
-from make_docs import HtmlBuild 
+from setuptools.command.develop import develop
+import os
+import zipfile
 
+
+# Pull the description values for the setup keywords from our file docstring.
+DOCLINES = __doc__.split("\n")
+
+
+# Setup our extensions to Python.
 def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration(None, parent_package, top_path)
@@ -50,14 +64,16 @@ packages = setuptools.find_packages(exclude=config['packages'] +
     ['docs', 'examples'])
 config['packages'] += packages
 
+
+# Function to build docs from source when building this project.
 def generate_docs():
-    """If sphinx is installed, generate docs.
+    """ If sphinx is installed, generate docs.
     """
     doc_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'docs')
     source_dir = os.path.join(doc_dir, 'source')
     html_zip = os.path.join(doc_dir,  'html.zip')
     dest_dir = doc_dir
-    
+
     required_sphinx_version = "0.4.1"
     sphinx_installed = False
     try:
@@ -65,21 +81,23 @@ def generate_docs():
         sphinx_installed = True
     except (DistributionNotFound, VersionConflict):
         log.warn('Sphinx install of version %s could not be verified.'
-                    ' Trying simple import...' % required_sphinx_version)
+            ' Trying simple import...' % required_sphinx_version)
         try:
             import sphinx
-            if parse_version(sphinx.__version__) < parse_version(required_sphinx_version):
-                log.error("Sphinx version must be >=%s." % required_sphinx_version)
+            if parse_version(sphinx.__version__) < parse_version(
+                required_sphinx_version):
+                log.error("Sphinx version must be >=" + \
+                    "%s." % required_sphinx_version)
             else:
                 sphinx_installed = True
         except ImportError:
             log.error("Sphnix install not found.")
-    
-    if sphinx_installed:             
+
+    if sphinx_installed:
         log.info("Generating %s documentation..." % INFO['name'])
         docsrc = source_dir
         target = dest_dir
-        
+
         try:
             build = HtmlBuild()
             build.start({
@@ -92,11 +110,11 @@ def generate_docs():
                 'versioned': False
                 }, [])
             del build
-            
+
         except:
             log.error("The documentation generation failed.  Falling back to "
-                      "the zip file.")
-            
+                "the zip file.")
+
             # Unzip the docs into the 'html' folder.
             unzip_html_docs(html_zip, doc_dir)
     else:
@@ -105,8 +123,7 @@ def generate_docs():
         unzip_html_docs(html_zip, doc_dir)
 
 def unzip_html_docs(src_path, dest_dir):
-    """Given a path to a zipfile, extract
-    its contents to a given 'dest_dir'.
+    """ Given a path to a zipfile, extract its contents to a given 'dest_dir'.
     """
     file = zipfile.ZipFile(src_path)
     for name in file.namelist():
@@ -124,18 +141,34 @@ def unzip_html_docs(src_path, dest_dir):
 class my_develop(develop):
     def run(self):
         develop.run(self)
-        # Generate the documentation.
         generate_docs()
 
 class my_build(distbuild):
     def run(self):
         distbuild.run(self)
-        # Generate the documentation.
         generate_docs()
 
+
+# The actual setup call.
 setup(
     author = 'Enthought, Inc',
     author_email = 'info@enthought.com',
+    classifiers = [c.strip() for c in """\
+        Development Status :: 4 - Beta
+        Intended Audience :: Developers
+        Intended Audience :: Science/Research
+        License :: OSI Approved :: BSD License
+        Operating System :: MacOS
+        Operating System :: Microsoft :: Windows
+        Operating System :: OS Independent
+        Operating System :: POSIX
+        Operating System :: Unix
+        Programming Language :: C
+        Programming Language :: Python
+        Topic :: Scientific/Engineering
+        Topic :: Software Development
+        Topic :: Software Development :: Libraries
+        """.splitlines()],
     cmdclass = {
         'develop': my_develop,
         'build': my_build
@@ -143,21 +176,25 @@ setup(
     dependency_links = [
         'http://code.enthought.com/enstaller/eggs/source',
         ],
-    description = "Science and mathematics features",
+    description = DOCLINES[1],
     extras_require = INFO['extras_require'],
     include_package_data = True,
     install_requires = INFO['install_requires'],
     license = "BSD",
+    long_description = '\n'.join(DOCLINES[3:]),
+    maintainer = 'ETS Developers',
+    maintainer_email = 'enthought-dev@enthought.com',
     name = INFO['name'],
     namespace_packages = [
         "enthought",
         ],
+    platforms = ["Windows", "Linux", "Mac OS-X", "Unix", "Solaris"],
     tests_require = [
         'nose >= 0.10.3',
         ],
     test_suite = 'nose.collector',
     url = 'http://code.enthought.com/ets',
     version = INFO['version'],
-    zip_safe = False, 
+    zip_safe = False,
     **config
     )
