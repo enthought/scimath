e1c93a8ec7896aa912dc567be767292f93fa9961
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-10-13 16:20:56 +0000

    * Updated to depend on new version of AppTools.

--- setup_data.py
+++ setup_data.py
@@ -14,7 +14,7 @@ def etsdep(p, min, max=None, literal=False):
 
 
 # Declare our ETS project dependencies.
-APPTOOLS = etsdep('AppTools', '3.0.1')
+APPTOOLS = etsdep('AppTools', '3.1.0')
 BLOCKCANVAS = etsdep('BlockCanvas', '3.0.1')
 ENTHOUGHTBASE_TRAITS = etsdep('EnthoughtBase[traits]', '3.0.1')
 ENVISAGECORE = etsdep('EnvisageCore', '3.0.1')
