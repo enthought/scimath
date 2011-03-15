51d9c10626d8b03f8dfc1c0833e4f48ff0fe0a6a
Ilan Schnell <ischnell@enthought.com>
2011-03-15 12:36:44 -0500

    cleanup (and test commit)

--- enthought/units/tests/units_dimensionless_test.py
+++ enthought/units/tests/units_dimensionless_test.py
@@ -7,18 +7,18 @@ def dimensionless_test():
     Test the modification to the division, multiplication and pow
     such that a dimensionless quantity formed by is indeed dimensionless
     """
-    
-    
+
+
     a = UnitScalar(1.0, units = 'm')
     b = UnitScalar(2.0, units = 'mm')
     d = UnitScalar(2.0, units = 'm**(-1)')
-    
+
     c = a/b
-    e = b*d 
+    e = b*d
 
     f= UnitScalar(2.0, units = dimensionless)
     g = f**2
-    
+
     assert_equal(c.units,dimensionless)
     assert_equal(e.units,dimensionless)
     assert_equal(g.units,dimensionless)
