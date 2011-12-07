21ad013bbb6f042c7eaedf1a224f4c4ae0fdee90
Tim Diller <timdiller@gmail.com>
2011-12-07 09:32:52 -0600

    initial pages commit

--- searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({objects:{},terms:{content:0,scimath:0,welcom:0,scientif:0,support:0,project:0,packag:0,calcul:0,mathemat:0,includ:0,interpol:0,document:0,unit:0,enthought:0},objtypes:{},titles:["Welcome to the SciMath documentation!"],objnames:{},filenames:["index"]})
