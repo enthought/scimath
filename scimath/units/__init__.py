215391b6dd6b0313e57e0ef63703c0263b590e2c
Tim Diller <timdiller@gmail.com>
2011-12-13 09:04:21 -0600

    added documentation website to comment string

--- scimath/units/__init__.py
+++ scimath/units/__init__.py
@@ -10,11 +10,13 @@
 #
 #  Enthought physical 'units' package
 #  Adds UnitManager et al
-#  (c) Copyright 2005 by Enthought Inc.
+#  (c) Copyright 2005-11 by Enthought Inc.
 #
 #-------------------------------------------------------------------------------
 """ Unit definitions and management utilities.
+
     Part of the SciMath project of the Enthought Tool Suite.
+    http://github.enthought.com/scimath/units/index.html
 """
 from convert import convert, parser
 from scimath.units.unit_manager import unit_manager
