c64df42f76a40caba7d466e380f370186a5c0f59
Prabhu Ramachandran <prabhu@enthought.com>
2013-03-29 00:52:29 +0530

    REL: Update version for 4.1.2 release.

--- scimath/__init__.py
+++ scimath/__init__.py
@@ -1,7 +1,7 @@
-# Copyright (c) 2007-2012 by Enthought, Inc.
+# Copyright (c) 2007-2013 by Enthought, Inc.
 # All rights reserved.
 
-__version__ = '4.1.1'
+__version__ = '4.1.2'
 
 __requires__ = [
     'traits',
