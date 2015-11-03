0358775a027b8f9f660b5f9ce4a472255d8f0d25
Jonathan Rocher <jrocher@enthought.com>
2015-11-03 11:20:25 -0600

    Clarify test intent and docstrings.

--- scimath/units/tests/test_custom_unit_parser.py
+++ scimath/units/tests/test_custom_unit_parser.py
@@ -3,16 +3,20 @@
 import unittest
 
 from scimath.units.api import unit_parser
-from scimath.units.length import mm
 from scimath.units.unit_parser import UnableToParseUnits
 from scimath.units.tests import sample_units
+
 
 class TestCustomUnitParser(unittest.TestCase):
 
     def test_extend_from_module(self):
-        # Custom units are not known initially
+        # Custom unit is not known initially
         with self.assertRaises(UnableToParseUnits):
             unit_parser.parse_unit("custom_unit", suppress_unknown=False)
 
         unit_parser.parser.extend(sample_units)
-        self.assertEqual(unit_parser.parse_unit("cuwl"), mm)
+
+        # Once parser is extended, the unit can be parsed
+        expected_unit = sample_units.custom_unit
+        self.assertEqual(unit_parser.parse_unit("custom_unit"), expected_unit)
+        self.assertEqual(unit_parser.parse_unit("cuwl"), expected_unit)
