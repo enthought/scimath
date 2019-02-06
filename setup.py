70d5d27d1bae41b16a235e53a19bd8020bc8dd1a
Poruri Sai Rahul <rporuri@enthought.com>
2019-02-06 17:31:10 +0530

    DEV : Bump version number back to dev
    Set the version number to 4.3.0 dev and flip the is_released
    boolean flag back to False
    
    	modified:   setup.py

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ import numpy.distutils.core
 
 
 MAJOR = 4
-MINOR = 2
+MINOR = 3
 MICRO = 0
-IS_RELEASED = True
+IS_RELEASED = False
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 VERSION_FILE_TEMPLATE = """\
 # This file is generated from setup.py
