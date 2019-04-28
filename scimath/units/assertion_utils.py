fbbc66e694d6492d3d09513ba2e862be9ddbde75
Jonathan Rocher <jrocher@kbibiopharma.com>
2019-04-28 17:54:03 -0500

    One more conversion eps -> rtol, and update test module.

--- scimath/units/assertion_utils.py
+++ scimath/units/assertion_utils.py
@@ -11,7 +11,7 @@ from scimath.units.compare_units import unit_arrays_almost_equal, \
 def assert_unit_scalar_almost_equal(val1, val2, rtol=1.e-9, msg=None):
     if msg is None:
         msg = "{} and {} are not almost equal with precision {}"
-        msg = msg.format(val1, val2, eps)
+        msg = msg.format(val1, val2, rtol)
 
     assert_true(unit_scalars_almost_equal(val1, val2, rtol=rtol), msg=msg)
 
@@ -19,7 +19,7 @@ def assert_unit_scalar_almost_equal(val1, val2, rtol=1.e-9, msg=None):
 def assert_unit_scalar_not_almost_equal(val1, val2, rtol=1.e-9, msg=None):
     if msg is None:
         msg = "{} and {} unexpectedly almost equal with precision {}"
-        msg = msg.format(val1, val2, eps)
+        msg = msg.format(val1, val2, rtol)
 
     assert_false(unit_scalars_almost_equal(val1, val2, rtol=rtol), msg=msg)
 
@@ -27,7 +27,7 @@ def assert_unit_scalar_not_almost_equal(val1, val2, rtol=1.e-9, msg=None):
 def assert_unit_array_almost_equal(uarr1, uarr2, rtol=1e-9, msg=None):
     if msg is None:
         msg = "{} and {} are not almost equal with precision {}"
-        msg = msg.format(uarr1, uarr2, eps)
+        msg = msg.format(uarr1, uarr2, rtol)
 
     assert_true(unit_arrays_almost_equal(uarr1, uarr2, rtol=rtol), msg=msg)
 
@@ -35,6 +35,6 @@ def assert_unit_array_almost_equal(uarr1, uarr2, rtol=1e-9, msg=None):
 def assert_unit_array_not_almost_equal(uarr1, uarr2, rtol=1e-9, msg=None):
     if msg is None:
         msg = "{} and {} are almost equal with precision {}"
-        msg = msg.format(uarr1, uarr2, eps)
+        msg = msg.format(uarr1, uarr2, rtol)
 
     assert_false(unit_arrays_almost_equal(uarr1, uarr2, rtol=rtol), msg=msg)
--- scimath/units/tests/test_assertion_utils.py
+++ scimath/units/tests/test_assertion_utils.py
@@ -1,8 +1,8 @@
 from unittest import TestCase
 
 from scimath.units.api import UnitArray, UnitScalar
-from scimath.units.testing.assertion_utils import \
-    assert_unit_array_almost_equal, assert_unit_scalar_almost_equal
+from scimath.units.assertion_utils import assert_unit_array_almost_equal, \
+    assert_unit_scalar_almost_equal
 
 
 class TestAssertUnitScalarEqual(TestCase):
@@ -14,6 +14,21 @@ class TestAssertUnitScalarEqual(TestCase):
         assert_unit_scalar_almost_equal(UnitScalar(1, units="m"),
                                         UnitScalar(100, units="cm"))
 
+    def test_not_close(self):
+        with self.assertRaises(AssertionError):
+            assert_unit_scalar_almost_equal(UnitScalar(1, units="m"),
+                                            UnitScalar(1.1, units="m"))
+
+    def test_not_close_custom_msg(self):
+        a1 = UnitScalar(1, units="m")
+        a2 = UnitScalar(1.1, units="m")
+        with self.assertRaises(AssertionError):
+            assert_unit_scalar_almost_equal(a1, a2, rtol=1e-2, msg="BLAH")
+
+    def test_unit_scalar_non_default_rtol(self):
+        assert_unit_scalar_almost_equal(UnitScalar(1, units="m"),
+                                        UnitScalar(1.01, units="m"), rtol=1e-1)
+
 
 class TestAssertUnitArrayEqual(TestCase):
     def test_same_unit_array(self):
@@ -23,3 +38,19 @@ class TestAssertUnitArrayEqual(TestCase):
     def test_equivalent_unit_array(self):
         assert_unit_array_almost_equal(UnitArray([1, 2], units="m"),
                                        UnitArray([100, 200], units="cm"))
+
+    def test_not_close(self):
+        a1 = UnitArray([1.01, 2], units="s")
+        a2 = UnitArray([1, 2], units="s")
+        with self.assertRaises(AssertionError):
+            assert_unit_array_almost_equal(a1, a2)
+
+    def test_not_close_custom_msg(self):
+        a1 = UnitArray([1.01, 2], units="s")
+        a2 = UnitArray([1, 2], units="s")
+        with self.assertRaises(AssertionError):
+            assert_unit_array_almost_equal(a1, a2, msg="BLAH")
+
+    def test_unit_scalar_non_default_rtol(self):
+        assert_unit_array_almost_equal(UnitScalar(1, units="m"),
+                                       UnitScalar(1.01, units="m"), rtol=1e-1)
