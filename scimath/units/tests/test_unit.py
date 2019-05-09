7aada675640cbe9dad663223223aae8ad6922d91
Poruri Sai Rahul <rporuri@enthought.com>
2019-05-09 13:10:33 +0530

    FIX : Test
    the test itself was broken. fixed it to work as needed
    
    	modified:   scimath/units/tests/test_unit.py

--- scimath/units/tests/test_unit.py
+++ scimath/units/tests/test_unit.py
@@ -7,7 +7,7 @@ class TestUnit(unittest.TestCase):
     def test_hashability_of_unit(self):
         unit_label_mapping = {
             meter: 'a meter',
-            centimeter: 'a centimerer'
+            centimeter: 'a centimeter'
         }
-        self.assertTrue(unit_label_mapping[meter], 'a meter')
-        self.assertTrue(unit_label_mapping[centimeter], 'a centimeter')
+        self.assertEqual(unit_label_mapping[meter], 'a meter')
+        self.assertEqual(unit_label_mapping[centimeter], 'a centimeter')
