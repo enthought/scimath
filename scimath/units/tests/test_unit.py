f5d54fb33678f1c9de1c317722e77fa97fd0cdd2
Poruri Sai Rahul <rporuri@enthought.com>
2019-05-09 13:14:26 +0530

    FIX : Add failing test
    There is a fundamental bug - in Python, if two objects compare equal,
    their hash values should be equal. The current implementation of
    __hash__ does not provide this behavior.
    
    The test creates two objects that compare equal and should therefore
    have equal hash values, which is tested.
    
    	modified:   scimath/units/tests/test_unit.py

--- scimath/units/tests/test_unit.py
+++ scimath/units/tests/test_unit.py
@@ -1,6 +1,7 @@
 import unittest
 
 from scimath.units.length import centimeter, meter
+from scimath.units.unit import unit
 
 
 class TestUnit(unittest.TestCase):
@@ -11,3 +12,9 @@ class TestUnit(unittest.TestCase):
         }
         self.assertEqual(unit_label_mapping[meter], 'a meter')
         self.assertEqual(unit_label_mapping[centimeter], 'a centimeter')
+
+    def test_units_should_hash_equal_if_they_compare_equal(self):
+        meter = unit(1.0, (1, 0, 0, 0, 0, 0, 0))
+        metre = unit(1.0, (1, 0, 0, 0, 0, 0, 0))
+        self.assertEqual(meter, metre)
+        self.assertEqual(hash(meter), hash(metre))
