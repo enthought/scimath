06e9af6a21df2bbecd0540d9e717a5a1fd19ea46
19leunam93 <39154009+19leunam93@users.noreply.github.com>
2019-07-02 14:35:39 +0200

    add some aliases

--- scimath/units/force.py
+++ scimath/units/force.py
@@ -13,9 +13,16 @@
 from __future__ import absolute_import
 from copy import copy
 
-from .SI import newton
+from .SI import newton, milli, kilo, mega
 
 N = newton
+millinewton = milli * newton
+kilonewton = kilo * newton
+meganewton = mega * newton
+
+mN = millinewton
+kN = kilonewton
+MN = meganewton
 
 lbf = 4.44822 * newton
 lbf.label = 'lbf'
