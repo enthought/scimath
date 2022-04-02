b5c950d8ed5300164ea667d5dd0211b8003d1e1c
sadra <lnxpylnxpy@gmail.com>
2022-04-02 21:47:19 +0430

    using process_time other than clock

--- scimath/interpolate/tests/test_basic.py
+++ scimath/interpolate/tests/test_basic.py
@@ -29,9 +29,9 @@ class Test(unittest.TestCase):
     def test_linear(self):
         y = arange(self.N)
         new_x = arange(self.N) + 0.5
-        t1 = time.clock()
+        t1 = time.process_time()
         new_y = linear(self.x, y, new_x)
-        t2 = time.clock()
+        t2 = time.process_time()
 
         print('1d interp (sec):', t2 - t1)
         self.assertAllclose(new_y[:5], [0.5, 1.5, 2.5, 3.5, 4.5])
@@ -39,9 +39,9 @@ class Test(unittest.TestCase):
     def test_block_average_above(self):
         y = arange(self.N)
         new_x = arange(self.N / 2) * 2
-        t1 = time.clock()
+        t1 = time.process_time()
         new_y = block_average_above(self.x, y, new_x)
-        t2 = time.clock()
+        t2 = time.process_time()
 
         print('1d block_average_above (sec):', t2 - t1)
         self.assertAllclose(new_y[:5], [0.0, 0.5, 2.5, 4.5, 6.5])
@@ -49,9 +49,9 @@ class Test(unittest.TestCase):
     def test_linear2(self):
         y = ones((100, self.N)) * arange(self.N)
         new_x = arange(self.N) + 0.5
-        t1 = time.clock()
+        t1 = time.process_time()
         new_y = linear(self.x, y, new_x)
-        t2 = time.clock()
+        t2 = time.process_time()
 
         print('fast interpolate (sec):', t2 - t1)
         self.assertAllclose(new_y[:5, :5],
@@ -64,10 +64,10 @@ class Test(unittest.TestCase):
     def test_interp1d(self):
         y = ones((100, self.N)) * arange(self.N)
         new_x = arange(self.N)
-        t1 = time.clock()
+        t1 = time.process_time()
         interp = scipy.interpolate.interp1d(self.x, y)
         new_y = interp(new_x)
-        t2 = time.clock()
+        t2 = time.process_time()
 
         print('scipy interp1d (sec):', t2 - t1)
         self.assertAllclose(new_y[:5, :5],
