178f1f07e5f4996c6da43cd6bdda01b082801c34
Corran Webster <cwebster@enthought.com>
2022-08-16 14:13:47 +0100

    Update copyright headers. (#157)

--- scimath/interpolate/interpolate.h
+++ scimath/interpolate/interpolate.h
@@ -1,5 +1,5 @@
 /*
-    (C) Copyright 2005-2021 Enthought, Inc., Austin, TX
+    (C) Copyright 2005-2022 Enthought, Inc., Austin, TX
     All rights reserved.
 
     This software is provided without warranty under the terms of the BSD
@@ -20,7 +20,7 @@
 template <class T>
 void linear(T* x_vec, T* y_vec, npy_intp len,
             T* new_x_vec, T* new_y_vec, npy_intp new_len)
-{    
+{
     for (npy_intp i=0;i<new_len;i++)
     {
        T new_x = new_x_vec[i];
@@ -30,18 +30,18 @@ void linear(T* x_vec, T* y_vec, npy_intp len,
        else if (new_x >=x_vec[len-1])
            index = len-2;
        else
-       { 
+       {
            T* which = std::lower_bound(x_vec, x_vec+len, new_x);
-           index = which - x_vec-1;            
+           index = which - x_vec-1;
        }
-             
+
        if(new_x == x_vec[index])
        {
            // exact value
            new_y_vec[i] = y_vec[index];
        }
        else
-       {     
+       {
            //interpolate
            double x_lo = x_vec[index];
            double x_hi = x_vec[index+1];
@@ -56,7 +56,7 @@ void linear(T* x_vec, T* y_vec, npy_intp len,
 template <class T>
 void loginterp(T* x_vec, T* y_vec, npy_intp len,
                T* new_x_vec, T* new_y_vec, npy_intp new_len)
-{    
+{
     for (npy_intp i=0;i<new_len;i++)
     {
         T new_x = new_x_vec[i];
@@ -66,18 +66,18 @@ void loginterp(T* x_vec, T* y_vec, npy_intp len,
         else if (new_x >=x_vec[len-1])
             index = len-2;
         else
-        { 
+        {
             T* which = std::lower_bound(x_vec, x_vec+len, new_x);
-            index = which - x_vec-1;            
+            index = which - x_vec-1;
         }
-             
+
         if(new_x == x_vec[index])
         {
             // exact value
             new_y_vec[i] = y_vec[index];
         }
         else
-        {     
+        {
             //interpolate
             double x_lo = x_vec[index];
             double x_hi = x_vec[index+1];
@@ -92,7 +92,7 @@ void loginterp(T* x_vec, T* y_vec, npy_intp len,
 template <class T>
 npy_intp block_average_above(T* x_vec, T* y_vec, npy_intp len,
                              T* new_x_vec, T* new_y_vec, npy_intp new_len)
-{    
+{
     npy_intp bad_index = -1;
     npy_intp start_index = 0;
     T last_y = 0.0;
@@ -109,19 +109,19 @@ npy_intp block_average_above(T* x_vec, T* y_vec, npy_intp len,
         else if (new_x == x_vec[0])
         {
             // for the first sample, just return the cooresponding y value
-            new_y_vec[i] = y_vec[0];             
-        }        
+            new_y_vec[i] = y_vec[0];
+        }
         else
         {
             T* which = std::lower_bound(x_vec, x_vec+len, new_x);
             npy_intp index = which - x_vec-1;
-           
+
             // calculate weighted average
-            
+
             // Start off with "residue" from last interval in case last x
             // was between to samples.
             T weighted_y_sum = last_y * thickness;
-            T thickness_sum = thickness;  
+            T thickness_sum = thickness;
             for(npy_intp j=start_index; j<=index; j++)
             {
                     if (x_vec[j+1] < new_x)
@@ -130,14 +130,14 @@ npy_intp block_average_above(T* x_vec, T* y_vec, npy_intp len,
                         thickness = new_x -x_vec[j];
                     weighted_y_sum += y_vec[j] * thickness;
                     thickness_sum += thickness;
-            }       
-            new_y_vec[i] = weighted_y_sum/thickness_sum; 
-            
+            }
+            new_y_vec[i] = weighted_y_sum/thickness_sum;
+
             // Store the thickness between the x value and the next sample
             // to add to the next weighted average.
             last_y = y_vec[index];
             thickness = x_vec[index+1] - new_x;
-            
+
             // start next weighted average at next sample
             start_index =index+1;
         }
@@ -149,13 +149,13 @@ template <class T>
 int window_average(T* x_vec, T* y_vec, npy_intp len,
                    T* new_x_vec, T* new_y_vec, npy_intp new_len,
                    T width)
-{    
+{
     for(npy_intp i=0;i<new_len;i++)
     {
         T new_x = new_x_vec[i];
         T bottom = new_x - width/2;
         T top = new_x + width/2;
-            
+
         T* which = std::lower_bound(x_vec, x_vec+len, bottom);
         npy_intp bottom_index = which - x_vec;
         if (bottom_index < 0)
@@ -163,7 +163,7 @@ int window_average(T* x_vec, T* y_vec, npy_intp len,
             //bottom = x_vec[0];
             bottom_index = 0;
         }
-        
+
         which = std::lower_bound(x_vec, x_vec+len, top);
         npy_intp top_index = which - x_vec;
         if (top_index >= len)
@@ -189,16 +189,16 @@ int window_average(T* x_vec, T* y_vec, npy_intp len,
                      "index: " << j << " " <<
                      "bottom: " << bottom << " " <<
                      "x+1: " << x_vec[j+1] << " " <<
-                     "x: " << x_vec[j] << " " << 
+                     "x: " << x_vec[j] << " " <<
                      "y: " << y_vec[j] << " " <<
-                     "weighted_sum: " << weighted_y_sum << 
+                     "weighted_sum: " << weighted_y_sum <<
                      "thickness: " << thickness << " " <<
                      "thickness_sum: " << thickness_sum << std::endl;
             */
-            //std::cout << x_vec[j] << " ";         
-            //std::cout << thickness << " ";         
+            //std::cout << x_vec[j] << " ";
+            //std::cout << thickness << " ";
         }
-       
+
         // last element
         thickness = top - bottom;
         weighted_y_sum += y_vec[top_index] * thickness;
@@ -206,14 +206,14 @@ int window_average(T* x_vec, T* y_vec, npy_intp len,
             /*
             std::cout <<  "iter: last" << " " <<
                      "index: " << top_index << " " <<
-                     "x: " << x_vec[top_index] << " " << 
+                     "x: " << x_vec[top_index] << " " <<
                      "y: " << y_vec[top_index] << " " <<
-                     "weighted_sum: " << weighted_y_sum << 
+                     "weighted_sum: " << weighted_y_sum <<
                      "thickness: " << thickness << " " <<
                      "thickness_sum: " << thickness_sum << std::endl;
             */
-            //std::cout << x_vec[top_index] << " " <<  thickness_sum << std::endl;   
-        new_y_vec[i] = weighted_y_sum/thickness_sum; 
+            //std::cout << x_vec[top_index] << " " <<  thickness_sum << std::endl;
+        new_y_vec[i] = weighted_y_sum/thickness_sum;
     }
     return -1;
 }
